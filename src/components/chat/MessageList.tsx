f7c1910ce9378b9e029a3be6648b49d470c124d3
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-08-18 10:40:26 +0530

    Add the message-list

---
+++ src/components/chat/MessageList.tsx
@@ -0,0 +1,108 @@
+import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { AwaitedReactNode, JSXElementConstructor, Key, ReactElement, ReactNode, useEffect, useRef } from "react";
+
+
+const MessageList = () => {
+	const {  } = ();
+	const { user: currentUser, isLoading: isUserLoading } = useKindeBrowserClient();
+	const messageContainerRef = useRef<HTMLDivElement>(null);
+
+	const { data: messages, isLoading: isMessagesLoading } = useQuery({
+		queryKey: ["messages", selectedUser?.id],
+		queryFn: async () => {
+			if (selectedUser && currentUser) {
+				return await messages(selectedUser?.id, currentUser?.id);
+			}
+		},
+		enabled: !!selectedUser && !!currentUser && !isUserLoading,
+	});
+
+	// Scroll to the bottom of the message container when new messages are added
+	useEffect(() => {
+		if (messageContainerRef.current) {
+			messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
+		}
+	}, [messages]);
+
+	return (
+		<div ref={messageContainerRef} className='w-full overflow-y-auto overflow-x-hidden h-full flex flex-col'>
+			{/* This component ensure that an animation is applied when items are added to or removed from the list */}
+			<AnimatePresence>
+				{!isMessagesLoading &&
+					messages?.map((message: { senderId: any; messageType: string; content: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | Promise<AwaitedReactNode> | null | undefined; }, index: Key | null | undefined) => (
+						<motion.div
+							key={index}
+							layout
+							initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
+							animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
+							exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
+							transition={{
+								opacity: { duration: 0.1 },
+								layout: {
+									type: "spring",
+									bounce: 0.3,
+									duration: messages.indexOf(message) * 0.05 + 0.2,
+								},
+							}}
+							style={{
+								originX: 0.5,
+								originY: 0.5,
+							}}
+							className={cn(
+								"flex flex-col gap-2 p-4 whitespace-pre-wrap",
+								message.senderId === currentUser?.id ? "items-end" : "items-start"
+							)}
+						>
+							<div className='flex gap-3 items-center'>
+								{message.senderId === selectedUser?.id && (
+									<Avatar className='flex justify-center items-center'>
+										<AvatarImage
+											src={selectedUser?.image}
+											alt='User Image'
+											className='border-2 border-white rounded-full'
+										/>
+									</Avatar>
+								)}
+								{message.messageType === "text" ? (
+									<span className='bg-accent p-3 rounded-md max-w-xs'>{message.content}</span>
+								) : (
+									<img
+										src={message.content}
+										alt='Message Image'
+										className='border p-2 rounded h-40 md:h-52 object-cover'
+									/>
+								)}
+
+								{message.senderId === currentUser?.id && (
+									<Avatar className='flex justify-center items-center'>
+										<AvatarImage
+											src={currentUser?.picture || "/user-placeholder.png"}
+											alt='User Image'
+											className='border-2 border-white rounded-full'
+										/>
+									</Avatar>
+								)}
+							</div>
+						</motion.div>
+					))}
+
+				{isMessagesLoading && (
+					<>
+						
+					</>
+				)}
+			</AnimatePresence>
+		</div>
+	);
+};
+export default MessageList;
+
+function useKindeBrowserClient(): { user: any; isLoading: any; } {
+    throw new Error("Function not implemented.");
+}
+function useQuery(arg0: { queryKey: any[]; queryFn: () => Promise<any>; enabled: boolean; }): { data: any; isLoading: any; } {
+    throw new Error("Function not implemented.");
+}
+
