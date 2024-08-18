fddcd159053efd0e1b81f6174d939a38589c5e5a
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-08-18 19:29:28 +0530

    Add the full design of sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,6 +7,9 @@ import { cn } from "@/lib/utils";
 import { LogOut } from "lucide-react";
 import useSound from "use-sound";
 import { usePreferences } from "@/store/usePreferences";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { useSelectedUser } from "@/store/useSelectedUser";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 interface SidebarProps {
 	isCollapsed: boolean;
@@ -16,7 +19,9 @@ interface SidebarProps {
 const Sidebar = ({ isCollapsed, users }: SidebarProps) => {
 	const [playClickSound] = useSound("/sounds/mouse-click.mp3");
 	const { soundEnabled } = usePreferences();
+	const { setSelectedUser, selectedUser } = useSelectedUser();
 
+	const { user } = useKindeBrowserClient();
 
 	return (
 		<div className='group relative flex flex-col h-full gap-4 p-2 data-[collapsed=true]:p-2  max-h-full overflow-auto bg-background'>
@@ -37,7 +42,7 @@ const Sidebar = ({ isCollapsed, users }: SidebarProps) => {
 									<div
 										onClick={() => {
 											soundEnabled && playClickSound();
-											
+											setSelectedUser(user);
 										}}
 									>
 										<Avatar className='my-1 flex justify-center items-center'>
@@ -59,15 +64,16 @@ const Sidebar = ({ isCollapsed, users }: SidebarProps) => {
 					) : (
 						<Button
 							key={idx}
-							
+							variant={"grey"}
+							size='xl'
 							className={cn(
 								"w-full justify-start gap-4 my-1",
-								
+								selectedUser?.email === user.email &&
 									"dark:bg-muted dark:text-white dark:hover:bg-muted dark:hover:text-white shrink"
 							)}
 							onClick={() => {
 								soundEnabled && playClickSound();
-								
+								setSelectedUser(user);
 							}}
 						>
 							<Avatar className='flex justify-center items-center'>
@@ -93,19 +99,21 @@ const Sidebar = ({ isCollapsed, users }: SidebarProps) => {
 						<div className='hidden md:flex gap-2 items-center '>
 							<Avatar className='flex justify-center items-center'>
 								<AvatarImage
-									
+									src={user?.picture || "/user-placeholder.png"}
 									alt='avatar'
 									referrerPolicy='no-referrer'
 									className='w-8 h-8 border-2 border-white rounded-full'
 								/>
 							</Avatar>
 							<p className='font-bold'>
-							
+								{user?.given_name} {user?.family_name}
 							</p>
 						</div>
 					)}
 					<div className='flex'>
-						
+						<LogoutLink>
+							<LogOut size={22} cursor={"pointer"} />
+						</LogoutLink>
 					</div>
 				</div>
 			</div>
